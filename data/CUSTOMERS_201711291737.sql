d58c64883454915ea28be3f4c9c70b5b05d743fb
simhead <simhead@192.168.1.130>
2017-11-29 20:32:48 +1100

    new release 1.0

---
+++ data/CUSTOMERS_201711291737.sql
@@ -0,0 +1,17 @@
+﻿INSERT INTO listtest.CUSTOMERS (CUSTOMERID,COUNTRY,DEBT,NAME,SALARY,ADDRESS,FIRSTNAME,LASTNAME) VALUES 
+(1,'UK',100,'Dave',500,'1 address city','Dave','dd')
+,(2,'Australia',500,'Peter',300,'2 address city','Peter','pp')
+,(3,'Switzerland',200,'Bob',300,'31 address city','Bob','bb')
+,(4,'Canada',10,'Eve',30,'41 address city','Eve','ee')
+,(5,'USA',300,'Carol',250,'51 address city','Carol','cc')
+,(6,'Switzerland',100,'Alice',200,'61 address city','Alice','aa')
+,(7,NULL,NULL,NULL,NULL,'test1 address','test1 firstname','test1 lastname')
+,(9,NULL,NULL,NULL,NULL,'null1112','Dave1112','dd1112')
+,(10,NULL,NULL,NULL,NULL,'test12 address','test12 firstname','test12 lastname')
+,(15,NULL,NULL,NULL,NULL,'changed null111234','changed Dave111234','changed d111243')
+;
+INSERT INTO listtest.CUSTOMERS (CUSTOMERID,COUNTRY,DEBT,NAME,SALARY,ADDRESS,FIRSTNAME,LASTNAME) VALUES 
+(16,NULL,NULL,NULL,NULL,'changed11 null111234','changed11 Dave111234','changed11 d111243')
+,(18,NULL,NULL,NULL,NULL,'BE null111234','ddDave111234','ddd111243')
+,(19,NULL,NULL,NULL,NULL,'Skagen 21 Stavanger 4006 Norway','NAVV','Tom B. Erichsen')
+;
